60d487c9f600f376e4326574d86c7963c5d144de
Judith van Stegeren <git@jd7h.com>
2016-10-05 16:58:12 +0200

    Grammar and generator

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # random-tony
-Generator for Tony Chocolonely flavours (in Dutch)
+Generator for limited edition Tony Chocolonely flavours (in Dutch)
+
+## Usage
+'''
+[chocoaddict@computer src]$ python grammar.py 
+wit-ananas-crumble
+puur-kokos-crumble-zeezout-karamel
+wit-ananas-sinaasappel-crumble
+melk-kersen-crumble-kers
+wit-hazelnoot
+puur-marsepein-hazelnoot-banaan
+melk-hazelnoot
+wit-macadamia-whiskey
+wit-peper
+melk-cola-pecan
+'''
+
+## Used sources
+Generating random sentences from a context free grammar - Eli Bendersky's website
+http://eli.thegreenplace.net/2010/01/28/generating-random-sentences-from-a-context-free-grammar/
---
+++ src/grammar.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+import random
+
+tony = """
+LIMITED_EDITION -> CHOCOLADESOORT SMAKEN
+SMAKEN -> SMAAK | SMAAK SMAAK | SMAAK SMAAK SMAAK
+SMAAK -> FRUIT | FRUITS CRUMBLE | NOTEN | DRANK | SNOEPGOED | KRUIDEN | OVERIG
+CRUMBLE -> crumble
+FRUIT -> sinaasappel | ananas | peer | 	banaan | kokos | kers | cranberry | rabarber
+FRUITS -> sinaasappel | ananas | peren | bananen | kokos | kersen | cranberry | rabarber
+NOTEN -> hazelnoot | macadamia | walnoot | pecan | amandel
+DRANK -> whiskey | rum | amaretto | cola
+SNOEPGOED -> marshmallow | marsepein | popcorn | discodip | noga | merengue | karamel
+KRUIDEN -> zeezout | peper | chili | rozemarijn | anijs
+OVERIG ->   honing | coffee-crunch 
+CHOCOLADESOORT -> melk | puur | wit
+"""
+
+class Grammar:
+	def __init__(self):
+		self.rules = {}
+
+	def add_rule(self,lhs,rhs):
+		self.rules[lhs] = []
+		rhs = rhs.split('|') # returns list of options
+		for r in rhs:
+			self.rules[lhs].append(r.split())
+
+	def generate(self,symbol):
+		sentence = []
+		choice = random.choice(self.rules[symbol]) # pick random
+		for part in choice:
+			if part in self.rules.keys():
+				sentence.append(self.generate(part))
+			else:
+				sentence.append(part)
+		return sentence
+
+	def parse_rule(self,line):
+		try:
+			lhs,rhs = line.split('->')
+		except:
+			return
+		lhs = lhs.strip()
+		rhs = rhs.strip()
+		self.add_rule(lhs,rhs)
+
+	def parse_grammar(self,grammarstring):
+		rules = grammarstring.split('\n')
+		for rule in rules:
+			self.parse_rule(rule)
+
+def flatten(foo):
+    for x in foo:
+        if hasattr(x, '__iter__') and not isinstance(x, str):
+            for y in flatten(x):
+                yield y
+        else:
+            yield x
+
+def generate_tony_flavours():
+	ldg = Grammar() # limited_edition_grammar
+	ldg.parse_grammar(tony)
+	for i in range(0,10):
+		print('-'.join(flatten(ldg.generate('LIMITED_EDITION'))))
+
+
+if __name__ == "__main__":
+    generate_tony_flavours()
